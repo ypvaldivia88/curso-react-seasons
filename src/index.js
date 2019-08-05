8fa4c2ac82d3aec3e1a6606955f0a0916d7dd347
Yasmani Palmero <yasmany.07@gmail.com>
2019-08-04 23:05:08 -0400

    se determino si es invierno o verano

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => { this.setState({ lat: position.coords.latitude });},
+            err => { this.setState({ errorMessage: err.message });} 
+        );
+    }
+
+    render() {   
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        
+        if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+        
+        return <div>Loading!</div>;
+
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
