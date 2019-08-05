49bef34a1541e17b9fa93446a673ca013e2ffeb9
Yasmani Palmero <yasmany.07@gmail.com>
2019-08-04 23:39:23 -0400

    agregados estilos

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,16 @@
+import './SeasonDisplay.css';
 import React from 'react';
+
+const seasonConfig = {
+    summer: {
+        text: 'Lets hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr, it is chilly',
+        iconName: 'snowflake'
+    }
+};
 
 const getSeason = (lat, month) => {
     if(month > 2 && month <9){
@@ -10,10 +22,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season];
 
-    console.log(season);
-
-    return <div>Season Display</div>;
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
